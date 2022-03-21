852974756d6b1f75c2fd36592572bd365150bcd0
KarlMarkusVaher <94621816+KarlMarkusVaher@users.noreply.github.com>
2022-03-21 18:37:33 +0200

    Add files via upload
    parem fail, saab mitu asju liigutada

--- n2.py
+++ n2.py
@@ -1,29 +1,51 @@
 from tkinter import *
+root = Tk(); root.geometry( "6000x4000" )
+c = Canvas( root, width = 8000, height = 4000, bg = "white" ); c.pack()
 
-root = Tk()
-root.title("Kala:)")
-root.geometry("800x600")
+class MainFrame:
+    """Class to move an image on a Canvas screen ( Python 2.5 )"""
+    def __init__( self, image0, image1 ):
+        self.__image0 = image0
+        self.__image1 = image1
+        self.__x, self.__y = 250,250
+        self.__picture0 = c.create_image( self.__x, self.__y,
+                                          image =  self.__image0 )
+        self.__picture1 = c.create_image( self.__x, self.__y,
+                                          image =  self.__image1 )
+        c.create_polygon((0, 100, 50, 0, 100, 100), fill="#4eccde")
+        self.__move = False
+        c.bind( "<Button-1>", self.startMovement )
+        c.bind( "<ButtonRelease-1>", self.stopMovement )
+        c.bind( "<Motion>", self.movement )
 
-w = 600
-h = 400
-x = w/2
-y = h/2
+    def startMovement( self, event ):
+        self.__move = True
+        self.initi_x = c.canvasx(event.x) #Translate mouse x screen coordinate to canvas coordinate
+        self.initi_y = c.canvasy(event.y) #Translate mouse y screen coordinate to canvas coordinate
+        print('startMovement init', self.initi_x, self.initi_y)
+        self.movingimage = c.find_closest(self.initi_x, self.initi_y, halo=5) # get canvas object ID of where mouse pointer is 
+        print(self.movingimage)
+        print(c.find_all()) # get all canvas objects ID 
 
-my_canvas = Canvas(root, width=2000, heigh=h, bg="white")
-my_canvas.pack(pady=20)
+    def stopMovement( self, event ):
+        self.__move = False
 
-img = PhotoImage(file="c:/Users/Karl-Markus/Pictures/Haug/Haug.png")
-my_image = my_canvas.create_image(260,125, image=img)
+    def movement( self, event ):
+        if self.__move:
+            end_x = c.canvasx(event.x) #Translate mouse x screen coordinate to canvas coordinate
+            end_y = c.canvasy(event.y) #Translate mouse y screen coordinate to canvas coordinate
+            print('movement end', end_x, end_y)
+            deltax = end_x - self.initi_x #Find the difference
+            deltay = end_y - self.initi_y
+            print('movement delta', deltax, deltay)
+            self.initi_x = end_x #Update previous current with new location
+            self.initi_y = end_y
+            print('movement init', self.initi_x, self.initi_y)
+            c.move(self.movingimage, deltax, deltay) # move object
 
-def move(e):
-    global img
-    img = PhotoImage(file="c:/Users/Karl-Markus/Pictures/Haug/Haug.png")
-    my_image = my_canvas.create_image(e.x,e.y, image=img)
-    my_label.config(text="Coordinates x: " + str(e.x) + " y: " + str(e.y))
-    
-my_label = Label(root, text="")
-my_label.pack(pady=20)
-
-my_canvas.bind("<B1-Motion>", move)
-
-root.mainloop()
+if __name__ == "__main__":      
+    im0 = PhotoImage( file = "c:/Users/Karl-Markus/Pictures/Haug/Haug.png" )
+    im1 = PhotoImage( file = "c:/Users/Karl-Markus/Pictures/Haug/wall.png" )
+#     im2 = PhotoImage( file = "giphy.gif" )
+    m = MainFrame( im0, im1)
+    mainloop()
